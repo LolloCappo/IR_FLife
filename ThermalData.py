1a88616ad814e414787bca3f2e7213ae9729156a
Lorenzo Capponi <lorenzocapponi@outlook.it>
2020-07-10 18:44:56 +0200

    update

--- ThermalData.py
+++ ThermalData.py
@@ -3,14 +3,59 @@ import matplotlib.pyplot as plt
 from tqdm import tqdm
 import FLife
 
-
-
 class ThermalData():
+
+    '''
+    Termoelasticity-based fatigue life identification.
+    The class takes in input a thermal-stress video and gives in output the fatigue life, estimanted via modal damage identification,
+    and via standard frequency-domain and time-domain methods. The natural frequencies to be used in modal damage identification
+    approach [1] can be also obtained through frequency analysis.
+
+    [1] Capponi, L., Slavič, J., Rossi, G., & Boltežar, M.
+        Thermoelasticity-based modal damage identification.
+        International Journal of Fatigue, 105661 (2020).
+    '''
+
+    def __init__(self, x, dt, nfi_pixel_x = None, nfi_pixel_y = None):
+
+        """ 
+        Get needed values
+
+        Parameters
+        ----------
+        x : array_like
+            Thermal video of stress measurement values. Correct shape: [frames, width, height]
+
+        dt : float
+            Time between discreete signal values 
         
-    def __init__(self, x, dt):
+        Methods
+        -------
+        ._find_nearest     : Find nearest element in array
+        
+        ._pixel_selection  : Storage of pixel coordinates
+        
+        .loc_selection     : Takes pixel coordinates and defines a roi location for the natural frequency identification
+        
+        .nf_identification : Natural frequency identification
+
+        .get_life          : Get the fatigue life
+
+        Raises
+        ------
+        ValueError :
+        ValueError :
+        ValueError : Frequency span around natural frequency must not be zero: set it to "None" or to a float value.
+        ValueError : Method must be one of: Modal, TovoBenasciutti, Dirlik, Rainflow
+        """
         
         self.x = x
         self.dt = dt
+        self.nfi_pixel_x = nfi_pixel_x
+        self.nfi_pixel_y = nfi_pixel_y
+
+        self.N = self.x.shape[0]
+        self.ds = self.x - self.x[0,:,:]
     
     def _find_nearest(self, array, value):
         
@@ -18,40 +63,91 @@ class ThermalData():
         idx = (np.abs(array - value)).argmin()
         
         return array[idx], idx
-    
+
     def _pixel_selection(self, event):
-        
-        global ix, iy
-        ix, iy = event.xdata, event.ydata
+        self.nfi_pixel_x, self.nfi_pixel_y = event.xdata, event.ydata
         plt.close()
+        return self.nfi_pixel_x, self.nfi_pixel_y
     
-    def nf_identification(self, location = None, high_pass = 5, low_pass = 100) :
-        
-        N = self.x.shape[0]
-        ds = self.x - self.x[0,:,:]
-                
-        if location is None:
-            global ix, iy
-            fig = plt.figure()
-            ax = fig.add_subplot(111)
-            ax.imshow(self.x[0,:,:])
-            fig.canvas.mpl_connect('button_press_event', self._pixel_selection)
-            plt.show()
-            (x, y, w, h) = (int(ix) - 2, int(iy) - 2, 5, 5)
+    def loc_selection(self):
+
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        ax.imshow(self.x[0,:,:])
+        fig.canvas.mpl_connect('button_press_event', self._pixel_selection)
+        plt.show()
+            
+    def nf_identification(self, location = None, roi = None, band_pass = None):
+
+        '''
+        Natural frequency identification through spectral analysis of the thermal video.
+
+        Parameters
+        ----------
+        location  : list, optional
+                    Thermal video of stress measurement values. Correct shape: [frames, width, height]
+
+        roi       : int, optional
+                    Pixel size of the region of interest for the natural frequency identification. If None is used, roi = 5.
+                    Default to None.
+
+        band_pass : list of float
+                    List of band pass filter frequencies for the natural frequency identification.
+                    If None is used, band_pass = [5, 100]. Default to None.
+
+        '''
+
+        if roi == None:
+            roi = 5
+
+        if band_pass == None:
+            band_pass = [5, 100]
+
+        if location is not None:
+            (x, y, w, h) = location
         else:
-            (x, y, w, h) = location    
-        
-        ds = ds[:, y:(y+h), x:(x+w)]
-                
-        fft = np.abs(np.fft.rfft(ds, N, axis = 0) * 2 / N)
-        freq = np.fft.rfftfreq(N, dt)
-        
-        y_peak = np.max(fft[(freq > high_pass) & (freq < low_pass)])
-        x_peak = freq[np.where(fft == y_peak)[0][0]]
-                        
-        return x_peak, y_peak
-    
+            (x, y, w, h) = (int(int(self.nfi_pixel_x) - (roi-1)/2), int(int(self.nfi_pixel_y) - (roi-1)/2), roi, roi)
+            
+        self.ds = self.ds[:, y:(y+h), x:(x+w)]
+
+        fft = np.abs(np.fft.rfft(self.ds, self.N, axis = 0) * 2 / self.N)
+        freq = np.fft.rfftfreq(self.N, self.dt)
+
+        ampl_nf = np.max(fft[(freq > band_pass[0]) & (freq < band_pass[1])])
+        nf = freq[np.where(fft == ampl_nf)[0][0]]
+
+        return np.round(nf, 2)    
+
     def get_life(self, C, k, method = None, f = None, location = None, f_span = None):
+
+
+        '''
+        Natural frequency identification through spectral analysis of the thermal video.
+
+        Parameters
+        ----------
+        C           : float
+                        Fatigue strength coefficient [MPa**k]
+
+        k           : float
+                        Fatigue strength exponent [/].
+
+        method      : string
+                        Method to use for the fatigue life identification.
+                        Method = ['Modal', 'TovoBenasciutti', 'Dirlik', 'Rainflow'] 
+
+        f           : float
+                        Natural frequency [Hz]. Necesary is method is 'Modal', otherwise is optional. 
+
+        location    : list, optional
+                        If the damage is wanted for a particular location
+
+        f_span      :   
+
+        '''
+        
+        self.N = self.x.shape[0]
+        self.ds = self.x - self.x[0,:,:]
         
         if method == None:
             method = 'TovoBenasciutti'
@@ -62,15 +158,12 @@ class ThermalData():
         if method not in ['Modal', 'TovoBenasciutti', 'Dirlik', 'Rainflow']:
             raise ValueError('Method must be one of: Modal, TovoBenasciutti, Dirlik, Rainflow')
         
-        N = self.x.shape[0]
-        ds = self.x - self.x[0,:,:]
-        
         if location is not None:
             (x, y, w, h) = location    
-            ds = ds[:, y:(y+h), x:(x+w)]
+            self.ds = self.ds[:, y:(y+h), x:(x+w)]
         
-        life = np.zeros(shape=(ds.shape[1], ds.shape[2]))
-        npixels = ds.shape[1] * ds.shape[2]  
+        life = np.zeros(shape=(self.ds.shape[1], self.ds.shape[2]))
+        npixels = self.ds.shape[1] * self.ds.shape[2]  
         
         if method == 'Modal':
             
@@ -78,11 +171,11 @@ class ThermalData():
                 raise ValueError('Natural frequency must be defined')
                 
             with tqdm(total = npixels) as pbar:   
-                for i in range(ds.shape[1]):
-                    for j in range(ds.shape[2]):    
+                for i in range(self.ds.shape[1]):
+                    for j in range(self.ds.shape[2]):    
                         
-                        fft  = np.abs(np.fft.rfft(ds[:,i,j], N) * 2 / N)
-                        freq = np.fft.rfftfreq(N, dt)
+                        fft  = np.abs(np.fft.rfft(self.ds[:,i,j], self.N) * 2 / self.N)
+                        freq = np.fft.rfftfreq(self.N, self.dt)
             
                         if f_span is not None:
                             x_peak = freq[(freq >= f - f_span) & (freq <= f + f_span)]
@@ -100,30 +193,30 @@ class ThermalData():
         elif method == 'TovoBenasciutti':
             
             with tqdm(total = npixels) as pbar:
-                for i in range(ds.shape[1]):
-                    for j in range(ds.shape[2]):
+                for i in range(self.ds.shape[1]):
+                    for j in range(self.ds.shape[2]):
                         
-                        tb = FLife.TovoBenasciutti(FLife.SpectralData(ds[:,i,j], self.dt))
+                        tb = FLife.TovoBenasciutti(FLife.SpectralData(self.ds[:,i,j], self.dt))
                         life[i,j] = tb.get_life(C = C, k = k, method = "method 2")
                         pbar.update(1)
                     
         elif method == 'Dirlik':
             
             with tqdm(total = npixels) as pbar:
-                for i in range(ds.shape[1]):
-                    for j in range(ds.shape[2]):
+                for i in range(self.ds.shape[1]):
+                    for j in range(self.ds.shape[2]):
                         
-                        dirlik = FLife.Dirlik(FLife.SpectralData(ds[:,i,j], self.dt))
+                        dirlik = FLife.Dirlik(FLife.SpectralData(self.ds[:,i,j], self.dt))
                         life[i,j] = dirlik.get_life(C = C, k = k)
                         pbar.update(1)
                     
         elif method == 'Rainflow':
             
             with tqdm(total = npixels) as pbar:    
-                for i in range(ds.shape[1]):
-                    for j in range(ds.shape[2]):
+                for i in range(self.ds.shape[1]):
+                    for j in range(self.ds.shape[2]):
                         
-                        rf = FLife.Rainflow(FLife.SpectralData(ds[:,i,j], self.dt))
+                        rf = FLife.Rainflow(FLife.SpectralData(self.ds[:,i,j], self.dt))
                         life[i,j] = rf.get_life(C = C, k = k)
                         pbar.update(1)
 
