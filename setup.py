daadba03269421ce4b03c89dc460113dbab36991
Lorenzo Capponi <52399106+LolloCappo@users.noreply.github.com>
2020-08-04 18:35:15 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,16 +5,15 @@ with open('README.rst', 'r', encoding='utf8') as f:
 from setuptools import setup, Extension
 from IR_FLife import __version__
 
-if __name__ == '__main__':
-    setup(
-        name='IR_FLife',
-        version=__version__,
-        author='Lorenzo Capponi',
-        author_email='lorenzocapponi@outlook.it',
-        description='Termoelasticity-based fatigue life identification',
-        url='https://github.com/LolloCappo/IR_FLife',
-        py_modules='IR_FLife',
-        long_description=readme,
-        long_description_content_type="text/markdown",
-        install_requires = ['numpy','tqdm','FLife']
-    )
+setup(
+    name='IR_FLife',
+    version=__version__,
+    author='Lorenzo Capponi',
+    author_email='lorenzocapponi@outlook.it',
+    description='Termoelasticity-based fatigue life identification',
+    url='https://github.com/LolloCappo/IR_FLife',
+    py_modules=['IR_FLife'],
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    install_requires = ['numpy','tqdm','FLife']
+)
